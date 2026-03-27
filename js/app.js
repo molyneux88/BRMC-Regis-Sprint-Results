faa65280d5284517d816ae1e69e0ba30af307327
luke.molyneux <molyneux_88@hotmail.com>
2026-03-27 16:43:58 +0100

    fixes

--- js/app.js
+++ js/app.js
@@ -624,20 +624,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-const burger = document.getElementById("burgerBtn");
-const mobileNav = document.getElementById("mobileNav");
-const closeBtn = document.getElementById("closeBurger");
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burgerBtn");
+  const mobileNav = document.getElementById("mobileNav");
+  const closeBtn = document.getElementById("closeBurger");
 
-// Toggle burger menu open
-burger.addEventListener("click", () => {
-  burger.classList.toggle("open");
-  mobileNav.classList.toggle("open");
-});
+  if (burger && mobileNav && closeBtn) {
+    // Toggle burger menu open
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("open");
+      mobileNav.classList.toggle("open");
+    });
 
-// Close button
-closeBtn.addEventListener("click", () => {
-  burger.classList.remove("open");
-  mobileNav.classList.remove("open");
+    // Close button
+    closeBtn.addEventListener("click", () => {
+      burger.classList.remove("open");
+      mobileNav.classList.remove("open");
+    });
+  }
 });
 
 //rebuild the table for mobile/desktop:
